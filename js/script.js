a658a936b58dbbbe0c70bc949a6d88b364c9e153
Skarlate Penelope <95501468+skarpenelope@users.noreply.github.com>
2024-08-02 17:06:04 +0000

    teste botão

--- js/script.js
+++ js/script.js
@@ -6,9 +6,39 @@ document.getElementById("valorEnt").innerHTML = `<i>${valorEntAtual}</i>`
 
 //dicionar nova linha na lista de entrada
 n = 0;
+const form = document.getElementById("list");
+const btn = document.getElementById("button+");
+let formCount = 0;
+
+/*function addlinha();
+
+document.addEventListener('DOMContentLoaded', function(){
+    addlinha.addEventListener('click', function(){
+        const newForm = document.createElement('form');
+        newForm.innerHTML = `
+        <fieldset>
+                    <form action="#">
+                        <input type="number" name="valor" id="value">
+                        <select name="fontes" id="meiodeent">
+                            <option value="trabalho">trabalho</option>
+                            <option value="vendas">vendas</option>
+
+                        </select>
+                        <input type="date" name="data" id="date">
+                        <button id="button+" onclick="addLinha()">+</button>
+
+                    </form>
+        </fieldset>`;
+                form.appendChild(newForm);
+
+        console.log("vc clicou no botão");
+    })
+})*/
+
+
 function addlinha(){
-    var element = document.getElementById("list");
-    var copy = document.cloneNode(element);
+    var linha = document.getElementById("list");
+    var copy = document.cloneNode(linha);
     n++;
 
     copy.getElementByTagName('valor');
