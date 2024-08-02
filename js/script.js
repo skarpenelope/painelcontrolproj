2777d51707ef4c1e5020d4d5c58b4398bf9e0b88
skar_penelope <penelopeskarlate@gmail.com>
2024-08-01 22:41:56 -0300

    add linha na ent e muda o valor total

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
+import react from React
+//mudar o valor total na página Entradas 
 const entrada = document.querySelector('destaque');
 
 let valorEntAtual = 20.000;
 
 document.getElementById("valorEnt").innerHTML = `<i>${valorEntAtual}</i>`
+
+//dicionar nova linha na lista de entrada
+function addlinha(){
+    var element = document.getElementById("list");
+    var copy = element.cloneNode(true);
+    n++;
+
+    copy.getElementByTagName('valor');
+    copy.getElementByTagName('fontes');
+    copy.getElementByTagName('data');
+
+    element.parentNode.appendChild(copy);
+
+}
